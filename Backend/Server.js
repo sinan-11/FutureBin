243be9eec2d9a5a38d4cba0f61771533f22e9f04
sinan-11 <sinanthaj17@gmail.com>
2026-08-05 19:03:58 +0530

    Dockerize FutureBin

--- Backend/Server.js
+++ Backend/Server.js
@@ -39,7 +39,7 @@ initSocket(server);
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
